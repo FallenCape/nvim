b03c5d972e1486341f4d28e361e850f804b8ccaf
Muhamed Heib <cyrexmaster@gmail.com>
2025-11-01 00:38:48 +0200

    Added NVim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,26 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.relativenumber = false
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.wrap = false
+
+opt.expandtab = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+
+opt.clipboard = "unnamedplus"
+
+opt.virtualedit = "block"
+
+opt.inccommand = "split"
+
+opt.ignorecase = true
+
+opt.termguicolors = true
